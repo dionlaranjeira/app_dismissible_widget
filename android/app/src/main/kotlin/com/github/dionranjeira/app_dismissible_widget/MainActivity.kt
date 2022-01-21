83dedbec7fe059a09e43eeb4d7f7c6f6b52ab79f
DION RIBEIRO LARANJEIRA <dionribeiro.rr@gmail.com>
2022-01-21 16:00:47 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/github/dionranjeira/app_dismissible_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.dionranjeira.app_dismissible_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:app_dismissible_widget/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
